97f162cd77cbf32ee1a2ec9cb2f1b76d36c51d03
Paulo <pauloaspinfa@gmail.com>
2020-09-16 22:01:02 -0300

    adicionando estrutura de dados fixa para teste de votaçao

---
+++ etapas.js
@@ -0,0 +1,51 @@
+let etapas = [
+    {
+        título: 'VEREADOR',
+        numeros:5,
+        candidatos:[
+                { numero: 38111,
+                name: 'Lula',
+                partido: 'PT',
+                foto:[{
+                    url:'download.jpeg', legenda:'vereador', small: false 
+                }]  
+                }
+            ]
+    },
+
+                { numero:77222,
+                name: 'jailson Mendes',
+                partido: 'PSDB',
+                fotos:[{
+                    url:'2.jpeg', legenda: 'Vereador'
+                }]
+                },
+
+    {
+        título: 'PREFEITO',
+        numero: 2,
+        candidatos: [
+            {
+                numero:99,
+                name:'fruit juice',
+                parido: 'sacomene',
+                vice:'Greg',
+                fotos:[
+                    {url:'fotochris', legenda: 'prefeito'},
+                    {url: 'fotogreg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            }
+        ]
+    },
+
+            {
+                numero: 10,
+                name: 'Dilma',
+                partido: 'Mandioca',
+                vice: 'Nando Moura',
+                fotos:[
+                    {url:'fotodilma', legenda:'Prefeito'},
+                    {url:'fotonando', legenda:'Vice-Prefeito'}
+                ]
+            }
+]
